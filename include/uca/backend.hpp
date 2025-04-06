aa1f8158830070efe165d1dae32279e0e48c46ee
like <462866046@qq.com>
2025-04-06 21:47:42 +0800

    add interface

--- include/uca/backend.hpp
+++ include/uca/backend.hpp
@@ -10,5 +10,9 @@ namespace uca
         void (*VtAdd)(const int n, const T *x, T *y);
         void (*integral_x)(vec2<size_t>, T*);
         void (*integral_y)(vec2<size_t>, T*);
+        void (*fft)(real_t<T>*, size_t* shape, size_t n);
+        void (*ifft)(real_t<T>*, size_t* shape, size_t n);
+        void (*fft_outplace)(complex_t<T>*, complex_t<T>*,size_t* shape, size_t n);
+        void (*ifft_outplace)(complex_t<T>*, complex_t<T>*, size_t* shape, size_t n);
     };
 }
